818ef81975589ac3f0b7d59941cc98c5781de3bc
Joshua Ly <josue.joshua.ly@gmail.com>
2022-07-26 15:50:09 -0500

    fixing transaction and monederoservice

---
+++ monedroService/src/test/java/com/bootcamp/monederoService/monedroService/MonedroServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.monederoService.monedroService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonedroServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ transactionService/src/main/java/com/bootcamp/transactionService/transactionService/repository/ITransactionRepository.java
@@ -0,0 +1,6 @@
+package com.bootcamp.transactionService.transactionService.repository;
+
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+
+public interface ITransactionRepository  extends ReactiveCrudRepository {
+}
