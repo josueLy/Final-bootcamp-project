2a9ffcce81f4a650643d5f17bb422916b435cce3
Josue Leon <jleonyal@emeal.nttdata.com>
2022-07-26 11:38:49 -0500

    first commit

---
+++ monedroService/src/main/java/com/bootcamp/monederoService/monedroService/MonedroServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bootcamp.monederoService.monedroService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonedroServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonedroServiceApplication.class, args);
+	}
+
+}
